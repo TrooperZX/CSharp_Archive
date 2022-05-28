8d5d3ddb2488d898a4cb273bb9c7d3bd7f626ca9
TrooperZX <TrooperZX@yandex.ru>
2022-05-28 15:25:30 +0300

    Homework2+ renaming of projects

---
+++ Homework2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Homework2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для постраения таблицы умножения:");
+
+            var InputNumber = 0;
+            var Input = Console.ReadLine();
+
+            bool userInput = int.TryParse(Input, out InputNumber);//check if input is number
+
+            if (userInput)
+            {
+                Console.WriteLine($"Введенное значение - число и равно {InputNumber}.Построение таблицы...");
+
+                for (int X = 0; X <= 10; X++)
+                {
+                    Console.WriteLine($"{Input} * {X} = {InputNumber * X}");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Введенное значение - не число");
+            }
+
+            Console.WriteLine("Конец программы");
+        }
+    }
+}
